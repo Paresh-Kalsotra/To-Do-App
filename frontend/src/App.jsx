db6c7b3693692a4e82b1bd98e0277264234e7508
Paresh-Kalsotra <pareshkalsotra@gmail.com>
2024-03-19 02:27:48 +0530

    server url updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -25,9 +25,7 @@ function App() {
   };
 
   return (
-    <div
-    // style={{ display: "flex", flexDirection: "column", alignItems: "center" }}
-    >
+    <div>
       <CreateTodo
         serverUrl={serverUrl}
         setTodos={setTodos}
